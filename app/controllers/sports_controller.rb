6c2089603ea6f4a61b7d7ff0e12bf9584c793e4b
Colin Bertin <colinbertin12@gmail.com>
2023-04-26 15:37:49 +0900

    add new file to create sport

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -1,9 +1,32 @@
 class SportsController < ApplicationController
+  # skip_before_action :authenticate_user!, only: [ :index, :show, :new, :create ] # :show if we want to
   def index
     @sports = Sport.all
   end
 
   def show
     @sport = Sport.find(params[:id])
+  end
+
+  def new
+    @sport = Sport.new
+    # authorize @sport
+  end
+
+  def create
+    @sport = Sport.new(sport_params)
+    # @sport.user = current_user
+    # authorize @sport
+    if @sport.save
+      redirect_to sports_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def sport_params
+    params.require(:sport).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'sports#index'
-  resources :sports, only: [:index, :show] do
+  resources :sports, only: [:index, :show, :new, :create] do
     resources :teams
   end
   # resources :teams, only: [:index, :show, :edit, :update]
