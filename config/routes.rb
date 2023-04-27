16f11827eae7b1379d4962abc70f14d8ee9d3fcb
Colin Bertin <colinbertin12@gmail.com>
2023-04-27 17:35:44 +0900

    add team show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'sports#index'
   resources :sports, only: [:index, :show, :new, :create] do
-    resources :teams, only: [:show, :new, :create]
+    resources :teams, only: [:new, :create]
   end
+  resources :teams, only: [:show]
 end
